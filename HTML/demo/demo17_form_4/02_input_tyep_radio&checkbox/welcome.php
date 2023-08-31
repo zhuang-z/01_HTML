c696589cf067969465aa16217d75f38c0bde2286
You <junyouzz@163.com>
2023-08-31 23:46:44 +0800

    work17_form4

---
+++ HTML/demo/demo17_form_4/02_input_tyep_radio&checkbox/welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>单选框演示</title>
+</head>
+<body>
+    <p>欢迎鱼油，您的表单已提交成功哦～</p>
+</body>
+</html>
---
+++ HTML/work/17_work_form_4/01_work_form/welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>多选框演示</title>
+</head>
+<body>
+    <p>您的表单已提交成功哦～</p>
+</body>
+</html>
